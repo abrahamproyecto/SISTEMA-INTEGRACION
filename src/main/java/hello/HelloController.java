aa5f7589f19c249e04c7da6d2a7ce0dd99627f7e
Abraham Proyecto <munozgarrid97@gmail.com>
2019-06-06 18:50:07 +0200

    modificacion en hello jva

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "HOLA MUND0, buenos días nuevamente";
+        return "HOLA MUND0, que esplendido dia";
     }
 
 }
